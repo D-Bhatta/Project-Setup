6a44d4309048def631ff61c5a56507dcea858b7d
D-Bhatta <dbhatta1232@gmail.com>
2020-08-18 08:43:58 -0400

    Add commands skeleton and test skeleton
    setup.py:
      - Update version to v0.0.3.0
    
    setup_project/project_name/commands.py -> setup_project/commands.py:
      - Move the commands.py file to the main project folder
    
    tests/test_commands.py:
      - Add test skeleton

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -7,52 +7,53 @@ import os
 import pytest
 
 import setup_project
-from setup_project import Commands
-
-os.chdir("tests/")
+from setup_project import Commands, commands
 
 
 def test_helloworld(capsys):
     """ Correct object argument prints """
-    Commands.helloworld("cat")
+    commands.helloworld("cat")
     captured = capsys.readouterr()
     assert "cat" in captured.out
 
 
 def test_helloworld_exception():
     with pytest.raises(TypeError):
-        Commands.helloworld(1)
+        commands.helloworld(1)
 
 
 class TestCommands(object):
     def test__init__(self):
+        x = Commands()
+        if not x.commands:
+            x.commands.append("command")
+            assert "command" in x.commands
+
+    def test_add_commands(self):
         pass
 
-    def test_add_commands(self, command_list):
-        pass
-
-    def test_add_commands_exception(self, command_list):
+    def test_add_commands_exception(self):
         pass
 
     def test_delete_commands(self):
         pass
 
-    def test_execute_cmd(self, command):
+    def test_execute_cmd(self):
         pass
 
-    def test_file_extract(self, file_name):
+    def test_file_extract(self):
         pass
 
-    def test_log_output(self, output):
+    def test_log_output(self):
         pass
 
     def test_run_commands(self):
         pass
 
-    def test_substitute_values(self, command_data):
+    def test_substitute_values(self):
         pass
 
-    def test_substitute_value(self, command, attribute):
+    def test_substitute_value(self):
         pass
 
     def test_operate(self):
